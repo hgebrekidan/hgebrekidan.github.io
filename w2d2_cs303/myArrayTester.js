67bc72ff50a187cbf6d13b8013284b297a759dce
Hailemariam Gebrekidan <midextam@gmail.com>
2021-03-30 22:50:52 -0500

    Update myArrayTester.js

--- w2d2_cs303/myArrayTester.js
+++ w2d2_cs303/myArrayTester.js
@@ -1,37 +1,43 @@
 "use strict";
 
-describe("higher oder practice", function () {
+describe("higher order functions map", function () {
  
-    it("multiply each element", function () {
+    it("create an array of a square of each elements", function () {
 
         assert.deepEqual(myMap([1, 2, 3, 4, 5],multiplyAll), [1, 4, 9, 16, 25])
  
     })
     
  
-    it("it sums values", function () {
+    it("create an array of a square of each elements", function () {
  
         assert.deepEqual(myMap([-1, -2, -3, -4, -5],multiplyAll), [1, 4, 9, 16, 25])
     })
  
 })
 
-describe("higher oder practice", function () {
+describe("higher order functions filter", function () {
  
-    it("multiply each element", function () {
+    it("create an array of odd values", function () {
         assert.deepEqual(myFilter([1, 2, 3, 4, 5],oddValue), [1, 3, 5])
  
     });
-    
+    it("create an array of odd values", function () {
+        assert.deepEqual(myFilter([17, 21, 28, 1003, 4, 52],oddValue), [17, 21, 1003])
+ 
+    });
  
 })
 
-describe("higher oder practice", function () {
+describe("higher order functions reduce", function () {
  
  
-    it("it sums values", function () {
+    it("sums up values of an array", function () {
  
         assert.deepEqual(myReduce([1, 2, 3, 4, 5],sumValue), 15)
     })
+    it("sums up values of an array", function () {
  
+        assert.deepEqual(myReduce([-1, 2, -3, 4, -5],sumValue), -3)
+    })
 });
