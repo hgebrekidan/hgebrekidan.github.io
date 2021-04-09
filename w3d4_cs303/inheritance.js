d0951297c1f48f45214e44f2716556ed96e972ee
Hailemariam Gebrekidan <midextam@gmail.com>
2021-04-09 00:54:55 -0500

    edited

--- w3d4_cs303/inheritance.js
+++ w3d4_cs303/inheritance.js
@@ -1,1 +1,20 @@
 "use strict";
+function drawBoard()
+{
+    let parent = document.getElementById("game");
+    let counter = 1;
+
+    for (let i = 0; i < 3; i++)
+    {
+        let row = document.createElement("tr");
+
+        for(let x = 0; x < size; x++)
+        {
+            let col = document.createElement("td");
+            col.innerHTML = counter;
+
+            row.appendChild(col);
+        }
+        parent.appendChild(row);
+    }
+}
